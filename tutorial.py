d428c8c9fe9a26c3cc4f97a9297b481033969384
SButterscotch <ssambhavv@gmail.com>
2021-08-12 21:18:01 +0545

    Second mod

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,19 @@
 # Let's start
+from flask import Flask, redirect, url_for
+
+site = Flask(__name__)
+
+@site.route("/")
+def home():
+    return "This is the Home Page"
+
+@site.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@site.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    site.run()
